a8cd61e25920fffedc5ad0edfafb32941700c8d6
ToanfKhoa <quachbuitoankhoa@gmail.com>
2025-04-12 23:31:53 +0700

    ParaGoomba class with basic function, state and the ability to fly
    Inherits from the Goomba class and does not have its own animation yet

---
+++ 05-SceneManager/ParaGoomba.cpp
@@ -0,0 +1,118 @@
+#include "ParaGoomba.h"
+#include "debug.h"
+
+CParaGoomba::CParaGoomba(float x, float y) : CGoomba(x, y)
+{
+	this->ax = 0;
+	this->ay = PARAGOOMBA_GRAVITY;
+	die_start = -1;
+	isGoomba = false;
+	SetState(PARAGOOMBA_STATE_WALKING);
+	fly_start = GetTickCount64();
+}
+
+void CParaGoomba::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+	if (state == GOOMBA_STATE_DIE)
+	{
+		left = x - PARAGOOMBA_BBOX_WIDTH / 2;
+		top = y - PARAGOOMBA_BBOX_HEIGHT_DIE / 2;
+		right = left + PARAGOOMBA_BBOX_WIDTH;
+		bottom = top + PARAGOOMBA_BBOX_HEIGHT_DIE;
+	}
+	else
+	{
+		left = x - PARAGOOMBA_BBOX_WIDTH / 2;
+		top = y - PARAGOOMBA_BBOX_HEIGHT / 2;
+		right = left + PARAGOOMBA_BBOX_WIDTH;
+		bottom = top + PARAGOOMBA_BBOX_HEIGHT;
+	}
+}
+
+void CParaGoomba::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	if (isGoomba == false)
+	{
+		vy += ay * dt;
+		vx += ax * dt;
+
+		//Fly if it's ParaGoomba
+		if(isGoomba == false)
+			Flying();
+
+		if (state == GOOMBA_STATE_DIE)
+		{
+			isDeleted = true;
+			return;
+		}
+
+		CGameObject::Update(dt, coObjects);
+		CCollision::GetInstance()->Process(this, dt, coObjects);
+	}
+	else
+		CGoomba::Update(dt, coObjects);
+}
+
+
+void CParaGoomba::Render()
+{
+	int aniId = ID_ANI_PARAGOOMBA_WALKING;
+	if (state == GOOMBA_STATE_DIE)
+	{
+		aniId = ID_ANI_GOOMBA_DIE;
+	}
+
+	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
+	RenderBoundingBox();
+}
+
+void CParaGoomba::SetState(int state)
+{
+	if (isGoomba == false)
+	{
+		CGameObject::SetState(state);
+		switch (state)
+		{
+		//ParaGoomba not affected by gravity while flying
+		case PARAGOOMBA_STATE_FLYING_UP:
+			vy = -PARAGOOMBA_FLYING_SPEED;
+			ay = 0;
+			break;
+		case PARAGOOMBA_STATE_FLYING_DOWN:
+			vy = PARAGOOMBA_FLYING_SPEED;
+			ay = 0;
+			break;
+		case PARAGOOMBA_STATE_WALKING:
+			vx = -GOOMBA_WALKING_SPEED;
+			break;
+		}
+	}
+	else
+	{
+		CGoomba::SetState(state);
+	}
+}
+
+//Check if the time is up and set flying state, this is called in update
+void CParaGoomba::Flying()
+{
+	DebugOut(L"ParaGoomba vy %.2f\n", vy);
+	DebugOut(L"State = %d\n", GetState());
+	if(GetState() == PARAGOOMBA_STATE_WALKING && GetTickCount64() - fly_start >= PARAGOOMBA_FLY_PERIOD)
+	{
+		SetState(PARAGOOMBA_STATE_FLYING_UP);
+		fly_start = GetTickCount64();
+	}
+
+	else if(GetState() == PARAGOOMBA_STATE_FLYING_UP && GetTickCount64() - fly_start >= PARAGOOMBA_FLY_TIME / 2)
+	{
+		SetState(PARAGOOMBA_STATE_FLYING_DOWN);
+		fly_start = GetTickCount64();
+	}
+
+	else if (GetState() != PARAGOOMBA_STATE_WALKING && GetTickCount64() - fly_start >= PARAGOOMBA_FLY_TIME)
+	{
+		SetState(PARAGOOMBA_STATE_WALKING);
+		fly_start = GetTickCount64();
+	}
+}
---
+++ 05-SceneManager/ParaGoomba.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "GameObject.h"
+#include "Goomba.h"
+
+#define PARAGOOMBA_GRAVITY 0.002f
+#define PARAGOOMBA_WALKING_SPEED 0.05f
+#define PARAGOOMBA_FLYING_SPEED 0.2f
+
+#define PARAGOOMBA_BBOX_WIDTH 16
+#define PARAGOOMBA_BBOX_HEIGHT 14
+#define PARAGOOMBA_BBOX_HEIGHT_DIE 7
+
+#define PARAGOOMBA_STATE_WALKING 300
+#define PARAGOOMBA_STATE_FLYING_UP 400
+#define PARAGOOMBA_STATE_FLYING_DOWN 500
+
+#define ID_ANI_PARAGOOMBA_WALKING 5000
+#define ID_ANI_PARAGOOMBA_FLYING 5001
+
+#define PARAGOOMBA_FLY_TIME 500
+#define PARAGOOMBA_FLY_PERIOD 1000
+
+class CParaGoomba : public CGoomba
+{
+protected:
+	void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	void Render();
+	boolean isGoomba;
+	ULONGLONG fly_start;
+	ULONGLONG fly_time;
+
+public:
+	CParaGoomba(float x, float y);
+	void SetState(int state);
+	void Flying();
+};
