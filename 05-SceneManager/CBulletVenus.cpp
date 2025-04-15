d7c5af371592cdb893f715f0b8ea93514d8e4d12
ToanfKhoa <quachbuitoankhoa@gmail.com>
2025-04-15 07:33:19 +0700

    Bullet of Venus class with basic function

---
+++ 05-SceneManager/CBulletVenus.cpp
@@ -0,0 +1,27 @@
+#include "CBulletVenus.h"
+
+void CBulletVenus::Render()
+{
+	RenderBoundingBox();
+	CAnimations::GetInstance()->Get(ID_ANI_BULLETVENUS)->Render(x, y);
+}
+
+void CBulletVenus::OnNoCollision(DWORD dt)
+{
+	x += vx * dt;
+	y += vy * dt;
+}
+
+void CBulletVenus::Fire(float direction_x, float direction_y)
+{
+	vx = direction_x * BULLET_SPEED;
+	vy = direction_y * BULLET_SPEED;
+}
+
+void CBulletVenus::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - BULLETVENUS_BBOX_WIDTH / 2;
+	t = y - BULLETVENUS_BBOX_HEIGHT / 2;
+	r = l + BULLETVENUS_BBOX_WIDTH;
+	b = t + BULLETVENUS_BBOX_HEIGHT;
+}
---
+++ 05-SceneManager/CBulletVenus.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+
+#define BULLET_SPEED 0.1f
+#define ID_ANI_BULLETVENUS 11000
+
+#define	BULLETVENUS_WIDTH 10
+#define BULLETVENUS_BBOX_WIDTH 10
+#define BULLETVENUS_BBOX_HEIGHT 16
+
+class CBulletVenus : public CGameObject {
+public:
+	CBulletVenus(float x, float y) : CGameObject(x, y) {}
+	void Render();
+	void Update(DWORD dt) {}
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+	void OnNoCollision(DWORD dt);
+	int IsBlocking() { return 0; }
+	void Fire(float direction_x, float direction_y);
+	void SetPostion(float x, float y) { this->x = x; this->y = y; }
+};
+
+typedef CBulletVenus* LPBULLETVENUS;
