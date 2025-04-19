c695e60605c8e373dac9ec060b7579173c34dec7
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2025-04-19 15:13:29 +0700

    Add state and movement logic for CCoinItem

--- 05-SceneManager/CCoinItem.cpp
+++ 05-SceneManager/CCoinItem.cpp
@@ -6,6 +6,29 @@ void CCoinItem::Render()
 	animations->Get(ID_ANI_COINITEM)->Render(x, y);
 }
 
+void CCoinItem::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	if (state == COINITEM_STATE_BOUNCING_UP)
+	{
+		vy = -COINITEM_BOUNCE_SPEED;
+	}
+	else if (state == COINITEM_STATE_BOUNCING_DOWN)
+	{
+		vy = COINITEM_BOUNCE_SPEED;
+	}
+
+	if (y <= y_start - COINITEM_BOUNCE_HEIGHT)
+	{
+		SetState(COINITEM_STATE_BOUNCING_DOWN);
+	}
+	else if (y >= y_start - COINITEM_BOUNCE_DELETE_HEIGHT && state == COINITEM_STATE_BOUNCING_DOWN)
+	{
+		isDeleted = true;
+	}
+
+	y += vy * dt;
+}
+
 void CCoinItem::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
 	l = x - COINITEM_BBOX_WIDTH / 2;
--- 05-SceneManager/CCoinItem.h
+++ 05-SceneManager/CCoinItem.h
@@ -7,12 +7,26 @@
 #define COINITEM_BBOX_WIDTH 10
 #define COINITEM_BBOX_HEIGHT 16
 
+#define COINITEM_STATE_IDLE 0
+#define COINITEM_STATE_BOUNCING_UP 1
+#define COINITEM_STATE_BOUNCING_DOWN 2
+
+#define COINITEM_BOUNCE_SPEED 0.18f
+#define COINITEM_BOUNCE_HEIGHT 64
+#define COINITEM_BOUNCE_DELETE_HEIGHT 16
+
+
 class CCoinItem :public CGameObject
 {
+	float y_start;
 public:
-	CCoinItem(float x, float y) : CGameObject(x, y) {}
+	CCoinItem(float x, float y) : CGameObject(x, y) 
+	{
+		y_start = y;
+		state = COINITEM_STATE_BOUNCING_UP;
+	}
 	void Render();
-	void Update(DWORD dt) {}
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects = NULL);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	int IsBlocking() { return 0; }
 };
