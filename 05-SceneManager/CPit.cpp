750cebd9a1047fe76fa12f83fb22de00ea486eb3
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2025-04-23 11:06:19 +0700

    Implement CPit class with basic attributes and methods based on sensor box

---
+++ 05-SceneManager/CPit.cpp
@@ -0,0 +1,30 @@
+#include "CPit.h"
+#include "Mario.h"
+
+CPit::CPit(float x, float y, int BBOX_WIDTH, int BBOX_HEIGHT)
+{
+	this->x = x;
+	this->y = y;
+	this->bboxWidth = BBOX_WIDTH;
+	this->bboxHeight = BBOX_HEIGHT;
+}
+
+void CPit::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
+void CPit::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - bboxWidth / 2;
+	t = y - bboxHeight / 2;
+	r = l + bboxWidth;
+	b = t + bboxHeight;
+}
+void CPit::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+	CMario* mario = dynamic_cast<CMario*>(e->obj);
+
+	mario->SetState(MARIO_STATE_DIE);
+}
--- 05-SceneManager/CPit.h
+++ 05-SceneManager/CPit.h
@@ -1,30 +1,19 @@
 #pragma once
 #include "GameObject.h"
-
-#define SENSORBOX_SPEED 0.02f
-#define SENSORBOX_GRAVITY 0.02f
-#define SENSORBOX_NO_COLLISION_THRESHOLD 1000
-
-class CSensorBox :public CGameObject
+class CPit :public CGameObject
 {
 protected:
-	float ay;
 	int bboxWidth;
 	int bboxHeight;
-	BOOLEAN isOnPlatform;
 
 public:
-	CSensorBox(float x, float y, int BBOX_WIDTH, int BBOX_HEIGHT);
+	CPit(float x, float y, int BBOX_WIDTH, int BBOX_HEIGHT);
 	void Render() { RenderBoundingBox(); }
 	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects = NULL);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 
-	void OnNoCollision(DWORD dt);
 	void OnCollisionWith(LPCOLLISIONEVENT e);
 	int IsBlocking() { return 0; }
 	int IsCollidable() { return 1; }
-
-	boolean getIsOnPlatform() { return isOnPlatform; };
-	float GetY() { return y; }
 };
 
