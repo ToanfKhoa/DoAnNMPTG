eb9e6e53ace481f933f9421da8105bce752ae15b
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2025-05-11 16:08:51 +0700

    Place coins in cloud area

--- 05-SceneManager/PlayScene.cpp
+++ 05-SceneManager/PlayScene.cpp
@@ -455,7 +455,7 @@ void CPlayScene::Update(DWORD dt)
 	if (cx < 0) cx = 0;
 	if (cx > END_OF_MAP) cx = END_OF_MAP;
 
-	CGame::GetInstance()->SetCamPos(cx, -100 /*cy*/);
+	CGame::GetInstance()->SetCamPos(cx, 0 /*cy*/);
 
 	PurgeDeletedObjects();
 }
