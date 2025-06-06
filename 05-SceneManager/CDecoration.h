5e076cd794e4238df642ec6307a9e183dd6db0fb
ToanfKhoa <quachbuitoankhoa@gmail.com>
2025-06-06 15:34:09 +0700

    Blur eye cloud with parallax

--- 05-SceneManager/CDecoration.h
+++ 05-SceneManager/CDecoration.h
@@ -1,5 +1,13 @@
 #pragma once
 #include "GameObject.h"
+#include "Game.h"
+#include "PlayScene.h"
+
+#define SPRITE_BLUR_EYE_CLOUD	102019
+#define SPRITE_EYE_CLOUD_FULL	102009
+#define SPRITE_EYE_CLOUD_MIDDLE	102010
+#define SPRITE_EYE_CLOUD_RIGHTHALF	102011
+
 class CDecoration : public CGameObject
 {
 protected:
@@ -8,6 +16,14 @@ public:
 	CDecoration(float x, float y, int spriteId) : CGameObject(x, y)
 	{
 		this->spriteId = spriteId;
+		
+		if (spriteId == SPRITE_EYE_CLOUD_FULL || spriteId == SPRITE_EYE_CLOUD_MIDDLE || spriteId == SPRITE_EYE_CLOUD_RIGHTHALF)
+			SetLayer(-10);
+		else if (spriteId == SPRITE_BLUR_EYE_CLOUD)
+			SetLayer(-11);
+
+		CPlayScene* playScene = dynamic_cast<CPlayScene*>(CGame::GetInstance()->GetCurrentScene());
+		playScene->SortObjects();
 	}
 
 	void Render();
